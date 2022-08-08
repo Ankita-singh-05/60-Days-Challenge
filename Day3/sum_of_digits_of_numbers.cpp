046d3bf40f10246ed0a24db5c82db4a728aae157
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-08-08 21:04:53 +0530

    Remaining ques of day3 of 60days

--- Day3/sum_of_digits_of_numbers.cpp
+++ Day3/sum_of_digits_of_numbers.cpp
@@ -1,1 +1,22 @@
 //Sum of digits of a number
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter any number:"<<endl;
+    cin>>num;
+    int sum = 0;
+
+
+    while(num!=0){
+             sum += num % 10;
+             num = num/10;
+    }
+
+
+    cout<<"Sum of digits of numbers is: "<<sum<<endl;
+
+    return 0;
+}
