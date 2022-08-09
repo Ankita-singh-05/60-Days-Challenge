1cf76c5366d9fa8c211e83993d0ec71a6d697d98
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-08-09 12:50:50 +0530

    Day 5 of 60days

---
+++ Day5/Prime_no_or_not.cpp
@@ -0,0 +1,42 @@
+//Prime number or not
+//Armstrong number or not
+//Strong number or not
+//Perfect number or not
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+
+    int num;
+    bool isprime = true;
+    cout<<"Enter any number:"<<endl;
+    cin>>num;
+
+
+    if(num<=2){
+        isprime = false;
+    }
+    else{
+        for(int i=2; i<sqrt(num); i++){
+        if(num%i==0){
+            isprime = false;
+            break;
+        }
+    }
+    }
+
+    if(isprime){
+        cout<<"Number is Prime!!"<<endl;
+    }
+    else{
+        cout<<"Number is Not Prime!!"<<endl;
+    }
+
+
+    return 0;
+}
+
+//Time complexity - O(root n)
+//Space complexity - O(1)
---
+++ Day5/palindrome_or_not.cpp
@@ -0,0 +1,30 @@
+//Palindrome or not
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num, reverse_no = 0, r;
+    cout<<"Enter any number: "<<endl;
+    cin>>num;
+
+    int temp = num;
+
+    while(temp != 0){
+        r = temp % 10;
+        reverse_no = reverse_no*10 + r;
+        temp = temp/10;
+    }
+
+
+
+    if(reverse_no == num){
+        cout<<"Number is Palindrome!!!"<<endl;
+    }
+    else{
+        cout<<"Not Palindrome!!!"<<endl;
+    }
+
+    return 0;
+}
