576b7596c598a39eabe1420c2027375bc75d126e
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-08-11 23:34:33 +0530

    Day 6

---
+++ Day6/Automorphic_or_not.cpp
@@ -0,0 +1,31 @@
+//Automorphic number or not
+
+
+// AUTOMORPHIC NUMBER - Number
+//Harshad number or not
+//Abundant number or not
+//Power of a number
+//Factors of a number
+//Add two fractions
+
+#include<iostream>
+using namespace std;
+
+int check
+
+int main(){
+
+    int num, r;
+    cout<<"Enter any number: "<<endl;
+    cin>>num;
+
+    int temp = num;
+
+
+    r = num % 10;
+    automorphic_no = num*num;
+
+
+
+    return 0;
+}
