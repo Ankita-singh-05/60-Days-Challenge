edabf0337f596c559e77aead6d46ac2665dfe6ff
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-08-14 12:49:36 +0530

    day 6 abundant number and power of a number

--- Day6/Abundant_Number_or_not.cpp
+++ Day6/Abundant_Number_or_not.cpp
@@ -1,3 +1,39 @@
 //Abundant number or not
 
+// ABUNDANT NUMBER or excessive number is a number for which the sum of its proper divisors is greater than the number
+
+#include<iostream>
+using namespace std;
+
+int abundantNumber(int num){
+
+    int temp = num;
+    int abundant = 0;
+
+    for(int i =1; i<num; i++){
+        if(num%i == 0){
+            abundant = abundant + i;
+        }
+    }
+    cout<<"Addition of numbers which are the proper divisors of the number is: "<<abundant<<endl;
+
+    if(abundant > temp){
+        cout<<"It is an Abundant Number!!!!"<<endl;
+    }
+    else{
+        cout<<"It is Not an Abundant Number!!!!"<<endl;
+    }
+}
+
+int main(){
+
+    int num;
+    cout<<"Enter any number: "<<endl;
+    cin>>num;
+
+    abundantNumber(num);
+
+    return 0;
+}
+
 
