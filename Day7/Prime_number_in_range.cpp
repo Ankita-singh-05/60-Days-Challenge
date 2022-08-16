01495c57f96a20ebfbc0fe7a1406fb04831db740
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-08-16 23:41:50 +0530

    Day7 prime no in given range

---
+++ Day7/Prime_number_in_range.cpp
@@ -0,0 +1,5 @@
+//Prime numbers in a given range
+//Armstrong numbers between two intervals
+//Can a number be expressed as a sum of two prime numbers?
+//Replace all 0’s with 1 in a given integer
+//Binary to decimal conversion
